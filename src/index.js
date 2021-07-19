c823f90a633169a7ce9b76aca9e9ff462677922c
jreynard <jeremy.reynard@cosmotech.com>
2021-07-19 15:16:27 +0200

    feat:  Add TranslationUtils

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ export { default as Auth } from './Auth';
 export { default as AuthDev } from './AuthDev';
 export { ScenarioUtils, DATASET_PARAM_VARTYPE } from './ScenarioUtils';
 export { WorkspaceFileUtils } from './WorkspaceFileUtils';
+export { TranslationUtils } from './TranslationUtils'
